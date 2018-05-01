5767566707d0a2081f403e1cdd215b3b8db02116
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-05-01 09:23:01 -0600

    Initial Kermit

---
+++ SpaceGame/Controller/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace SpaceGame
+{
+	public class Player
+	{
+		public Player()
+		{
+			public void Initialize()
+			{
+				
+			}
+
+			public void Update()
+			{
+				
+			}
+
+			public void Draw()
+			{
+				
+			}
+
+		}
+	}
+}
