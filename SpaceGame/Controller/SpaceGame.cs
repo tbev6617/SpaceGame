05c10a81ec5f18a406cd456930178231f6621027
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-05-02 09:51:31 -0600

    Added enemies but it doesn't work

--- SpaceGame/Controller/SpaceGame.cs
+++ SpaceGame/Controller/SpaceGame.cs
@@ -3,7 +3,9 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 using SpaceGame.Model;
+using SpaceGame.View;
 
 namespace SpaceGame.Controller
 {
@@ -15,11 +17,45 @@ namespace SpaceGame.Controller
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 		private Player player;
+
+		// Keyboard states used to determine key presses
+		private KeyboardState currentKeyboardState;
+		private KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		private GamePadState currentGamePadState;
+		private GamePadState previousGamePadState;
+
+		// A movement speed for the player
+		private float playerMoveSpeed;
+
+		// Enemies
+		private Texture2D enemyTexture;
+		private List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		private TimeSpan enemySpawnTime;
+		private TimeSpan previousSpawnTime;
+
+		// A random number generator
+		private Random random;
+
 		public SpaceGame()
 		{
 			player = new Player();
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
+			// Initialize the enemies list
+			enemies = new List<Enemy> ();
+
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
 		}
 
 		/// <summary>
@@ -30,7 +66,8 @@ namespace SpaceGame.Controller
 		/// </summary>
 		protected override void Initialize()
 		{
-			// TODO: Add your initialization logic here
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
 
 			base.Initialize();
 		}
@@ -44,11 +81,15 @@ namespace SpaceGame.Controller
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 			// Load the player resources 
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("Animation/shipAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
+
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
+			enemyTexture = Content.Load<Texture2D>("Animation/mineAnimation");
 
-			player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
-
-			//TODO: use this.Content to load your game content here 
 		}
 
 		/// <summary>
@@ -65,8 +106,19 @@ namespace SpaceGame.Controller
 				Exit();
 #endif
 
-			// TODO: Add your update logic here
+			// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
 
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+			//Update the player
+			UpdatePlayer(gameTime);
+			// Update the enemies
+			UpdateEnemies(gameTime);
 			base.Update(gameTime);
 		}
 
@@ -86,7 +138,84 @@ namespace SpaceGame.Controller
 			// Stop drawing 
 			spriteBatch.End();
 
+			// Draw the Enemies
+			for (int i = 0; i<enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
+
 			base.Draw(gameTime);
+		}
+		private void UpdatePlayer(GameTime gameTime)
+		{
+			player.Update(gameTime);
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+		}
+		private void AddEnemy()
+		{
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30, Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position);
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--)
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				}
+		 	}
 		}
 	}
 }
--- SpaceGame/Model/Player.cs
+++ SpaceGame/Model/Player.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SpaceGame.View;
+
 namespace SpaceGame.Model
 {
 	public class Player
 	{
 		public Vector2 Position;
 
-		private Texture2D playerTexture;
-		public Texture2D PlayerTexture
+		// Animation representing the player
+		private Animation playerAnimation;
+		public Animation PlayerAnimation
 		{
-			get { return playerTexture; }
-			set { playerTexture = value; }
+			get { return playerAnimation; }
+			set { playerAnimation = value; }
 		}
 
 		private bool active;
@@ -32,13 +35,13 @@ namespace SpaceGame.Model
 		// Get the width of the player ship
 		public int Width
 		{
-			get { return PlayerTexture.Width; }
+		get { return playerAnimation.FrameWidth; }
 		}
 
 		// Get the height of the player ship
 		public int Height
 		{
-			get { return PlayerTexture.Height; }
+			get { return playerAnimation.FrameHeight; }
 		}
 
 		public Player()
@@ -46,28 +49,31 @@ namespace SpaceGame.Model
 
 		}
 
-		public void Initialize(Texture2D texture, Vector2 position)
+		// Initialize the player
+		public void Initialize(Animation animation, Vector2 position)
 		{
-			PlayerTexture = texture;
+			playerAnimation = animation;
 
-			// Set the starting position of the player around the middle of the screen and to the back 
+			// Set the starting position of the player around the middle of the screen and to the back
 			Position = position;
 
-			// Set the player to be active 
+			// Set the player to be active
 			Active = true;
 
 			// Set the player health
 			Health = 100;
 		}
 
-		public void Update()
+		public void Update(GameTime gameTime)
 		{
-
+			playerAnimation.Position = Position;
+			playerAnimation.Update(gameTime);
 		}
 
+		// Draw the player
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			playerAnimation.Draw(spriteBatch);
 		}
 	}
 }
